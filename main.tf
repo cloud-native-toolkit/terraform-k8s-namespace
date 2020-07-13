7e08865208f72b93467b81b6fb45cec9e9b91c24
Sean Sundberg <seansund@us.ibm.com>
2020-07-13 09:27:01 -0500

    Replaces kubernetes namespace with script to control destroy logic (#5)

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-provider "kubernetes" {
-  config_path = var.cluster_config_file_path
-}
 provider "null" {}
 
 resource "null_resource" "delete_namespace" {
@@ -13,16 +10,34 @@ resource "null_resource" "delete_namespace" {
   }
 }
 
-resource "kubernetes_namespace" "tools" {
+resource "null_resource" "create_namespace" {
   depends_on = [null_resource.delete_namespace]
 
-  metadata {
-    name = var.name
+  triggers = {
+    name       = var.name
+    kubeconfig = var.cluster_config_file_path
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl create namespace ${self.triggers.name}"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
+    }
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "${path.module}/scripts/deleteNamespace.sh ${self.triggers.name}"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
+    }
   }
 }
 
 resource "null_resource" "copy_apikey_secret" {
-  depends_on = [kubernetes_namespace.tools]
+  depends_on = [null_resource.create_namespace]
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/copy-secret-to-namespace.sh ibmcloud-apikey ${var.name}"
@@ -34,7 +49,7 @@ resource "null_resource" "copy_apikey_secret" {
 }
 
 resource "null_resource" "copy_cloud_configmap" {
-  depends_on = [kubernetes_namespace.tools]
+  depends_on = [null_resource.create_namespace]
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/copy-configmap-to-namespace.sh ibmcloud-config ${var.name}"
@@ -46,7 +61,7 @@ resource "null_resource" "copy_cloud_configmap" {
 }
 
 resource "null_resource" "create_pull_secrets" {
-  depends_on = [kubernetes_namespace.tools]
+  depends_on = [null_resource.create_namespace]
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/setup-namespace-pull-secrets.sh ${var.name}"
