a322a18fec7ef7677e05ded4887f77fad1fee780
seansund <seansund@users.noreply.github.com>
2020-07-15 04:58:14 +0000

    deploy: f3cc3b1a663793842bc95c2690088db495195a54

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  value       = var.name
-  description = "Namespace name"
-  depends_on  = [
-    null_resource.create_pull_secrets,
-    null_resource.copy_cloud_configmap,
-    null_resource.copy_apikey_secret
-  ]
-}
