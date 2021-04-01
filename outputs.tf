45aeb56ee7fca3e28208df45e92d9c59f0c4e763
Sean Sundberg <seansund@us.ibm.com>
2021-04-01 03:43:24 -0500

    Removes missing dependency in output (#15)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,6 @@ output "name" {
   value       = var.name
   description = "Namespace name"
   depends_on  = [
-    null_resource.create_pull_secrets,
     null_resource.copy_cloudnative_configmaps,
     null_resource.copy_cloudnative_secrets
   ]
