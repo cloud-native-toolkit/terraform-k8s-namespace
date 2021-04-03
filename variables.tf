7083b93d26ae7103c68180b1f396ee2bc630776d
Sean Sundberg <seansund@us.ibm.com>
2021-04-03 18:17:10 -0500

    Removes unnecessary input variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- variables.tf
+++ variables.tf
@@ -7,14 +7,3 @@ variable "name" {
   type        = string
   description = "The namespace that should be created"
 }
-
-variable "cluster_type" {
-  type        = string
-  description = "The type of cluster that should be created (openshift or ocp3 or ocp4 or kubernetes)"
-}
-
-variable "tls_secret_name" {
-  type        = string
-  description = "The name of the secret containing the tls certificate for the cluster"
-  default     = ""
-}
