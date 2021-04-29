84672eb75dad35abab6bcc39f7bcc93ee1bee2a1
Sean Sundberg <seansund@us.ibm.com>
2021-04-29 03:08:53 -0500

    Updates test validation
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -2,4 +2,10 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
 
-exit 0
+KUBECONFIG="~/.kube/config"
+
+NAMESPACE=$(cat terraform.tfvars | grep namespace | sed -E "s/namespace += +\"(.*)\"/\1/g)
+
+echo "Checking for namespace: ${NAMESPACE}"
+
+kubectl get namespace "${NAMESPACE}"
