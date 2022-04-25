5ea59fa7a6de5d8a4c006115bdadca572850d21f
Andrew Trice <amtrice@us.ibm.com>
2022-04-25 12:53:36 -0400

    don't fail on existing namespace
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-namespaces.tf
+++ test/stages/stage2-namespaces.tf
@@ -4,3 +4,11 @@ module "dev_tools_namespace" {
   cluster_config_file_path = module.dev_cluster.config_file_path
   name                     = var.namespace
 }
+
+#handle duplicate namespace (already exists)
+module "dev_tools_namespace2" {
+  source = "./module"
+
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  name                     = var.namespace
+}
